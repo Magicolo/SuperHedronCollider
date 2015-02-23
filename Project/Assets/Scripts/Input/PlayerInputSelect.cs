73fab6ee063e064676af3eaf6c10e4a7cca079bf
Magicolo <straty91@hotmail.com>
2015-02-23 17:27:06 -0500

    jht

--- Project/Assets/Scripts/Input/PlayerInputSelect.cs
+++ Project/Assets/Scripts/Input/PlayerInputSelect.cs
@@ -7,9 +7,6 @@ public class PlayerInputSelect : State {
 	
 	[Disable] public Vector3 selectionStart;
 	
-	LineRenderer _lineRenderer;
-	public LineRenderer lineRenderer { get { return _lineRenderer ? _lineRenderer : (_lineRenderer = GetComponent<LineRenderer>()); } }
-	
 	PlayerInput Layer {
 		get { return ((PlayerInput)layer); }
 	}
@@ -17,17 +14,10 @@ public class PlayerInputSelect : State {
 	public override void OnEnter() {
 		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100));
 		selectionStart = mouseWorldPosition;
-		
-		lineRenderer.SetPosition(0, selectionStart);
-		lineRenderer.SetPosition(1, selectionStart);
-		lineRenderer.SetPosition(2, selectionStart);
-		lineRenderer.SetPosition(3, selectionStart);
-		lineRenderer.SetPosition(4, selectionStart);
-		lineRenderer.enabled = true;
 	}
 	
 	public override void OnExit() {
-		lineRenderer.enabled = false;
+		
 	}
 	
 	public override void OnUpdate() {
@@ -36,11 +26,13 @@ public class PlayerInputSelect : State {
 			return;
 		}
 		
-		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100));
-		lineRenderer.SetPosition(0, new Vector3(selectionStart.x, 10, selectionStart.z));
-		lineRenderer.SetPosition(1, new Vector3(mouseWorldPosition.x, 10, selectionStart.z));
-		lineRenderer.SetPosition(2, new Vector3(mouseWorldPosition.x, 10, mouseWorldPosition.z));
-		lineRenderer.SetPosition(3, new Vector3(selectionStart.x, 10, mouseWorldPosition.z));
-		lineRenderer.SetPosition(4, new Vector3(selectionStart.x, 10, selectionStart.z));
+		Vector3 startViewPosition = Camera.main.WorldToViewportPoint(selectionStart);
+		Vector3 mouseViewPosition = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+		Vector3[] points = new Vector3[4];
+		points[0] = new Vector3(startViewPosition.x, startViewPosition.y, 0);
+		points[1] = new Vector3(mouseViewPosition.x, startViewPosition.y, 0);
+		points[2] = new Vector3(mouseViewPosition.x, mouseViewPosition.y, 0);
+		points[3] = new Vector3(startViewPosition.x, mouseViewPosition.y, 0);
+		SelectionBoxRenderer.points = points;
 	}
 }
---
+++ Project/Assets/Scripts/Input/SelectionBoxRenderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Magicolo;
+
+public class SelectionBoxRenderer : MonoBehaviourExtended {
+
+	public static bool draw;
+	public static Vector3[] points = new Vector3[0];
+	
+	void OnPostRender() {
+		if (!draw) {
+			return;
+		}
+		
+		GL.Begin(GL.QUADS);
+		GL.Color(Color.green);
+        
+		foreach (Vector3 point in points) {
+			GL.Vertex3(point.x, point.y, point.z);
+		}
+		
+		GL.End();
+	}
+}
+
