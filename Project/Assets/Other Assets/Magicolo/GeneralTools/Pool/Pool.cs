1bfaeb1c2733ad29aa0b3610f0ee965894d77cd0
Magicolo <straty91@hotmail.com>
2015-02-23 12:17:34 -0500

    boba fett was here

---
+++ Project/Assets/Other Assets/Magicolo/GeneralTools/Pool/Pool.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Magicolo;
+using Magicolo.GeneralTools;
+
+[ExecuteInEditMode]
+public class Pool : MonoBehaviour {
+
+	static Pool instance;
+	static Pool Instance {
+		get {
+			if (instance == null) {
+				instance = FindObjectOfType<Pool>();
+			}
+			
+			return instance;
+		}
+	}
+	
+	public PoolPrefabManager prefabManager;
+	
+	public void Initialize() {
+		if (SingletonCheck()) {
+			return;
+		}
+	}
+	
+	public bool SingletonCheck() {
+		if (Instance != null && Instance != this) {
+			if (!Application.isPlaying) {
+				Logger.LogError(string.Format("There can only be one {0}.", GetType().Name));
+			}
+			
+			gameObject.Remove();
+				
+			return true;
+		}
+			
+		return false;
+	}
+
+	void Awake() {
+		Initialize();
+	}
+}
