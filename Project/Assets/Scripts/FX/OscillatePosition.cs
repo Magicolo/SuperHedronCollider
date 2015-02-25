bca7f1afe915de085d8fc5ba2ae727dc0651043e
straty91 <straty91@hotmail.com>
2015-02-25 12:37:11 -0500

    lights moar

--- Project/Assets/Scripts/FX/OscillatePosition.cs
+++ Project/Assets/Scripts/FX/OscillatePosition.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Magicolo;
 
-public class OscillateLight : MonoBehaviourExtended {
+public class OscillatePosition : MonoBehaviourExtended {
 
 	public float frequency = 1;
 	public float amplitude = 0.5F;
 	public float center = 0.5F;
 	
 	void Update() {
-		light.intensity = center + amplitude * Mathf.Sin(frequency * Time.time + GetInstanceID() / 1000);
+		transform.OscillateLocalPosition(frequency, amplitude, center, "Z");
 	}
 }
+
