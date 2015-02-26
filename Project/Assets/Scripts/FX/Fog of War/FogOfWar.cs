03995f46d06b372be0fc6eda7121fcc2b9fcea37
Richard Rail <rail.rich@gmail.com>
2015-02-26 00:19:09 -0500

    asd

--- Project/Assets/Scripts/FX/Fog of War/FogOfWar.cs
+++ Project/Assets/Scripts/FX/Fog of War/FogOfWar.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FogOfWar : MonoBehaviour {
 
-	public int precision = 1;
+	public float precision = 0.5f;
 	
 	public Camera sourceCamera;
 	
@@ -18,29 +18,35 @@ public class FogOfWar : MonoBehaviour {
 	}
 	
 	void Start () {
-		width = (int)gameObject.transform.localScale.x;
-		height = (int)gameObject.transform.localScale.z;
-		foWTexture = new Texture2D(width * precision, height * precision, TextureFormat.Alpha8, false);
+		/*width = (int)(gameObject.transform.localScale.x * precision);
+		height = (int)(gameObject.transform.localScale.z * precision);
+		foWTexture = new Texture2D(width , height , TextureFormat.RGBA32, false);
 		
 		Material material = GetComponent<MeshRenderer>().materials[0];
-		material.mainTexture = foWTexture;
+		material.mainTexture = foWTexture;*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		width = (int)(gameObject.transform.localScale.x * precision);
+		height = (int)(gameObject.transform.localScale.z * precision);
+		foWTexture = new Texture2D(width , height , TextureFormat.RGBA32, false);
+		
+		Material material = GetComponent<MeshRenderer>().materials[0];
+		material.mainTexture = foWTexture;
+		
+		
 		float[,] alphas = new float[width,height];
 		
 		foreach (var troop in TroopManager.GetTroops(NetworkController.CurrentPlayerId)) {
 			Vector3 dir = (troop.transform.position - sourceCamera.transform.position).normalized;
 			Ray johnRay = new Ray(sourceCamera.transform.position, dir);
-			Debug.DrawRay(sourceCamera.transform.position, dir * 1000, new Color(1,0.7f,0.2f, 1f));
 			RaycastHit toucheJohn;
 			if (Physics.Raycast(johnRay, out toucheJohn)){
 				Vector3 pointDeTouche = toucheJohn.point;
 				Point2D textureHit = worldToTexturePosition(pointDeTouche);
-				//troop.sightRadius
-				Debug.Log(pointDeTouche + " KWAME " + textureHit);
-				int range = 8;
+				int range = (int)(troop.sightRadius * width / 600.0f *4);
+				Debug.Log(troop.sightRadius * width + " - " + range);
 				int xMin = Mathf.Clamp(textureHit.x - range ,0,width);
 				int xMax = Mathf.Clamp(textureHit.x + range ,0,width);
 				int yMin = Mathf.Clamp(textureHit.y - range ,0,height);
@@ -48,8 +54,8 @@ public class FogOfWar : MonoBehaviour {
 				Vector2 pdt3 = new Vector2(textureHit.x, textureHit.y);
 				for (int x = xMin; x < xMax; x++) {
 					for (int y = yMin; y < yMax; y++) {
-						float distance = Vector2.Distance(new Vector2(x,y),pdt3);
-						alphas[x,y] = 1 - distance/range;;
+						float distance = Mathf.Abs(Vector2.Distance(new Vector2(x,y),pdt3));
+						alphas[x,y] = 1- distance/range;
 					}
 				}
 				
@@ -61,7 +67,7 @@ public class FogOfWar : MonoBehaviour {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				float alpha = alphas[x,y];
-				pixels[index] = new Color(0,0,0, 1- alpha);
+				pixels[index] = new Color(0,0,0, 1 - alpha);
 				index++;
 			}
 		}
@@ -70,12 +76,11 @@ public class FogOfWar : MonoBehaviour {
 	}
 
 	Point2D worldToTexturePosition(Vector3 pointDeTouche){
-		/*float floatyX = pointDeTouche.x + transform.localScale.x/2;
-		float floatyZ = pointDeTouche.z + transform.localScale.z/2;*/
-		float floatyX = pointDeTouche.x + 300;
-		float floatyZ = pointDeTouche.z + 75;
-		
-		return new Point2D((int)(floatyX/precision),(int)(floatyZ/precision));
+		float machinX = 300/2;
+		float machinY = 72/2;
+		float floatyX = (pointDeTouche.x + machinX) * width / 300;
+		float floatyZ = (pointDeTouche.z + machinY) * height / 75;
+		return new Point2D((int)(floatyX),(int)(floatyZ));
 		
 	}
 }
