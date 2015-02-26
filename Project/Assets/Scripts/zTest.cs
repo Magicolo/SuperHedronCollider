272cc7a591c682d2076c57e77dec09da069677ff
Magicolo <straty91@hotmail.com>
2015-02-25 19:32:26 -0500

    kjuuuuu

---
+++ Project/Assets/Scripts/zTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Magicolo;
+
+public class zTest : MonoBehaviour {
+
+	public int renderQueue = 1000;
+	
+	[Button("Test", "Test", NoPrefixLabel = true)] public bool test;
+	void Test() {
+		Logger.Log(3 / 2);
+	}
+	
+	void Start() {
+		GameManager.Start();
+	}
+}
